13881764f61dd54774db380e50bd4794cb952d9b
AnudeepK23 <90596066+AnudeepK23@users.noreply.github.com>
2022-03-11 11:29:13 +0530

    hello-app React

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css"
-import { ProductProvider } from './contextApi';
 
 ReactDOM.render(
-  <ProductProvider>
-    <Router>
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </Router>
-  </ProductProvider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
